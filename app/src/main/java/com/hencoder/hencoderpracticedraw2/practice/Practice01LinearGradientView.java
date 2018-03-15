775893dd3eebec96fda2fdda668c54e7e6a5bc9d
Pioneerz <690859614@qq.com>
2018-03-15 11:15:02 +0800

    test

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -10,6 +10,9 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+/**
+ * @author Administrator
+ */
 public class Practice01LinearGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice02RadialGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice02RadialGradientView.java
@@ -10,6 +10,9 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+/**
+ * @author Administrator
+ */
 public class Practice02RadialGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
